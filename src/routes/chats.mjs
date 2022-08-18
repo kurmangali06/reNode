77da3c4cefc4b17bddda7cc7febcc639df29586e
Курмангали <kurmangali0606@gmail.com>
2022-08-18 15:13:37 +0600

    homework 5

--- src/routes/chats.mjs
+++ src/routes/chats.mjs
@@ -4,28 +4,27 @@ import { Chats } from "../models/chats.mjs";
 const router = express.Router();
 
 router
-  .get('/', async (req, res) => {
-  const chats = await Chats.find()
-  res.json(chats)
+.get("/", async (req, res) => {
+const chats = await Chats.find();
+res.json(chats);
 })
-  .post('/', async (req, res) => {
-    try{
-  const newChats = await Chats.create(req.body)
-  res.status(201);
-  res.json(newChats);}
-  catch(err) {
-    res.status(500);
-    res.send(err)
-  }
+.post("/", async (req, res, next) => {
+await Chats.create(req.body, (err, newChats) => {
+if (err) {
+next(err);
+}
+res.status(201);
+res.json(newChats);
+});
 })
-  .delete('/:id', async (req, res) => {
-  const deleted = await Chats.findByIdAndDelete(req.params.id)
-  res.status(200)
-  res.json(deleted);
+.delete("/:id", async (req, res) => {
+const deleted = await Chats.findByIdAndDelete(req.params.id);
+res.status(200);
+res.json(deleted);
 })
-  .put('/:id', async (req, res) => {
-  const updateChats = await Chats.findByIdAndUpdate(req.params.id, req.body)
-  res.json(updateChats)
-})
+.put("/:id", async (req, res) => {
+const updateChats = await Chats.findByIdAndUpdate(req.params.id, req.body);
+res.json(updateChats);
+});
 
 export default router;
